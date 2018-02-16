90e5260db63c939a20a418948ab666747e2b0458
Matthias Hans <matthias.hans@enisyst.de>
2018-02-16 23:42:53 +0100

    added JUL logging functionality

--- src/Start.java
+++ src/Start.java
@@ -1,9 +1,17 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Start {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  private static final Logger log = Logger.getLogger( Start.class.getName() );
 
+	  public static void main( String[] args )
+	  {
+	    log.info( "starting BTD1" );
+
+	    
+	    
+
+	    log.info( "Ging alles glatt." );
+	  }
 	}
-
-}
