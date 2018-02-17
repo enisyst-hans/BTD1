7093da2c717d7fde931481bb0ca6ece203525688
Matthias Hans <matthias.hans@enisyst.de>
2018-02-17 23:53:26 +0100

    first brief of xml map

--- src/backend/Map.java
+++ src/backend/Map.java
@@ -1,10 +1,15 @@
 package backend;
 
+import java.util.logging.Logger;
+
+
 public class Map {
 
 	private Field[][] fields;
+	private static final Logger log = Logger.getLogger(Map.class.getName());
 	
 	public Map(int pXFields, int pYFields) {
 		fields = new Field[pXFields][pYFields];
+		log.info("generated map: ["+pXFields+"]["+pXFields+"]");
 	}
 }
