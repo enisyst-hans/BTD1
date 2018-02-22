ff16dbbdf14c3be3cf8de1ba684f57f64a13846f
Matthias Hans <matthias.hans@enisyst.de>
2018-02-22 21:05:18 +0100

    added function for generate new map

--- src/core/Map.java
+++ src/core/Map.java
@@ -9,6 +9,10 @@ public class Map {
 	private static final Logger log = Logger.getLogger(Map.class.getName());
 	
 	public Map(int pXFields, int pYFields) {
+		generateMap(pXFields, pYFields);
+	}
+	
+	public void generateMap(int pXFields, int pYFields) {
 		fields = new Field[pXFields][pYFields];
 		log.info("generated map: ["+pXFields+"]["+pXFields+"]");
 	}
