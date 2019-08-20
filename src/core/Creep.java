191326c4aea51d56ad6a54bbbe8ed0133d62204a
Matthias Hans <matthias.hans@enisyst.de>
2019-08-20 21:48:35 +0200

    added class for creeps and waves

---
+++ src/core/Creep.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Creep {
+
+	private int level = 0;
+	private int health = 0;
+	private int armor = 0;
+	private int speed = 0;
+	private int creepType =0;
+
+	public Creep(int pCreepType) {
+		creepType = pCreepType;
+	}
+}
---
+++ src/core/Wave.java
@@ -0,0 +1,13 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wave {
+
+	private List<Creep> creepList = new ArrayList<Creep>();
+
+	public Wave() {
+		super();
+	}
+}
